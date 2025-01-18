41dc27a2c11634656b63bde674a3b4ce8db2449b
Kaze Wong <kazewong.physics@gmail.com>
2025-01-17 22:26:58 -0500

    Rerun can video pose now

--- video_pose_overlay.py
+++ video_pose_overlay.py
@@ -7,15 +7,13 @@ from psycopg.rows import TupleRow
 from dotenv import load_dotenv
 import os
 import pickle
-import uuid
-import requests
-from io import BytesIO, BufferedReader
-
+import io
+import urllib.request
 
 # Load the environment variables from the .env file
 load_dotenv() 
 
-def get_postgres_client():
+def get_postgres_client() -> psycopg.connection:
     return psycopg.connect(
         host = os.getenv("POSTGRES_HOST"),
         port = os.getenv("POSTGRES_PORT"),
@@ -35,28 +33,42 @@ def get_minio_url(name: str):
     url = get_minio_client().presigned_get_object("highjump", name)
     return url
 
+def query_joint_point(video_name: str):
+    with get_postgres_client() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT frame_idx, position FROM video_yolo_pose WHERE video_name = %s", (video_name,))
+            row = cursor.fetchall()    
+            return row
 
 rr.init("rerun_asset_video_auto_frames")
 rr.serve_web(ws_port=4321, web_port=10000)
 
-tag = 'raw_data/2024/12/10/20241210_195854.mp4'
+tag = 'raw_data/2024/10/27/20241027_144541.mp4'
 url = get_minio_url(tag)
 
-print(url)
+urllib.request.urlretrieve(url, "./test.mp4")
+video_asset = rr.AssetVideo(path="./test.mp4")
 
-#response = requests.get(url)
-# bytes_io = BytesIO(response.content)
+# video_asset = rr.AssetVideo(path="./test.mp4")
 
-video_asset = rr.AssetVideo(path="./test.mp4")#contents=bytes_io)
+joint_point_data = query_joint_point(tag)
 
 rr.log("video", video_asset, static=True)
 frame_timestamps_ns = video_asset.read_frame_timestamps_ns()
+print(frame_timestamps_ns)
 rr.send_columns(
     "video",
     # Note timeline values don't have to be the same as the video timestamps.
     times=[rr.TimeNanosColumn("video_time", frame_timestamps_ns)],
     components=[rr.VideoFrameReference.indicator(), rr.components.VideoTimestamp.nanoseconds(frame_timestamps_ns)],
 )
+
+for (idx, joint_point) in joint_point_data:
+    rr.set_time_nanos('video_time', frame_timestamps_ns[idx])
+    joint_position = pickle.loads(joint_point)
+    for j in joint_position:
+        rr.log('joint', rr.Points2D(j[:, :2]))
+
 
 try:
   while True:
