a64ef886f3682bff973e3bb497de61eba2361c13
Kaze Wong <kazewong.physics@gmail.com>
2025-07-11 16:00:21 -0400

    update multiple videos view

--- multi_videos_view.py
+++ multi_videos_view.py
@@ -8,23 +8,26 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--folder", type=str, help="Folder name")
 args = parser.parse_args()
 
-folder = args.folder
-print(folder)
+file_list = os.listdir(args.folder)
 
-videos = os.listdir(folder)
+videos = [f for f in file_list if f.lower().endswith('.mp4')]
+time_csv = np.loadtxt(os.path.join(args.folder, "recordings.csv"), delimiter=',', skiprows=1, dtype=str)
+starting_timestamps = {}
+for row in time_csv:
+    starting_timestamps[row[0]] = float(row[1])
 
 rr.init("rerun_asset_video_auto_frames")
 rr.serve_web(ws_port=4321, web_port=10000)
-max_times = []
 
-for idx,video in enumerate(videos):
-    video_asset = rr.AssetVideo(path=folder + "/" + video)
-    max_times.append(video_asset.read_frame_timestamps_ns()[-1])
 
-for idx,video in enumerate(videos):
-    video_asset = rr.AssetVideo(path=folder + "/" + video)
+for idx, video in enumerate(videos):
+    # Extract IP address from the video filename (assumes IP is before first '_')
+    ip_addr = video.split('_')[0]
+    video_asset = rr.AssetVideo(path=os.path.join(args.folder, video))
     rr.log("video" + str(idx), video_asset, static=True)
-    frame_timestamps_ns = video_asset.read_frame_timestamps_ns() + (max(max_times) - max_times[idx])
+    # Use the IP address to get the starting timestamp
+    start_ts = starting_timestamps.get(ip_addr, 0)
+    frame_timestamps_ns = video_asset.read_frame_timestamps_ns() #+ start_ts
     rr.send_columns(
         "video" + str(idx),
         # Note timeline values don't have to be the same as the video timestamps.
